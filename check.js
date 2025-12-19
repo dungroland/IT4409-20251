2a36867a2eae0ae35f9183aa8e79e6ac30f4e8c3
dungroland <DUNG.NH235052@sis.hust.edu.vn>
2025-12-19 17:35:31 +0700

    Delete check.js

--- check.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  "mongodb+srv://20235052:20235052@cluster8.d2izqxc.mongodb.net/week10"
-)
-.then(() => console.log("✅ MongoDB connected"))
-.catch(err => console.error("❌ MongoDB error:", err));
